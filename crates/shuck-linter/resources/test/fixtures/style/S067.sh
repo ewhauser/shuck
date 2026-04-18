f80c672cd2ae9af38107260c4cebf1111d10a26d
Eric Hauser <ewhauser@gmail.com>
2026-04-18 15:38:02 -0600

    Sync S067 fixture snapshot with escaped grep pattern

--- crates/shuck-linter/resources/test/fixtures/style/S067.sh
+++ crates/shuck-linter/resources/test/fixtures/style/S067.sh
@@ -2,6 +2,7 @@
 
 grep '*MAINTAINER' "$AUDIT_FILE"
 grep "*ENTRYPOINT" "$AUDIT_FILE"
+grep \*MAINTAINER "$AUDIT_FILE"
 grep *CMD "$AUDIT_FILE"
 grep -e '*LABEL' "$AUDIT_FILE"
 grep --regexp '*EXPOSE' "$AUDIT_FILE"
@@ -9,7 +10,6 @@ grep -v '^*' "$AUDIT_FILE"
 
 grep 'MAINTAINER*' "$AUDIT_FILE"
 grep '.*MAINTAINER' "$AUDIT_FILE"
-grep \*MAINTAINER "$AUDIT_FILE"
 grep -F '*MAINTAINER' "$AUDIT_FILE"
 grep -e'*MAINTAINER' "$AUDIT_FILE"
 grep --regexp='*MAINTAINER' "$AUDIT_FILE"
