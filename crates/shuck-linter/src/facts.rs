cdcd42aa1a6e0607aa984a67a0a404e264c58277
Eric Hauser <ewhauser@gmail.com>
2026-04-18 18:04:34 -0600

    Fix C056 conformance

--- crates/shuck-linter/src/facts.rs
+++ crates/shuck-linter/src/facts.rs
@@ -3312,23 +3312,26 @@ impl<'a> LinterFactsBuilder<'a> {
                             &mut condition_status_capture_spans,
                         );
 
+                        let mut previous_condition = &command.condition;
                         for (condition, branch) in &command.elif_branches {
+                            collect_condition_status_capture_from_body(
+                                previous_condition,
+                                condition,
+                                self.source,
+                                &mut condition_status_capture_spans,
+                            );
                             collect_condition_status_capture_from_body(
                                 condition,
                                 branch,
                                 self.source,
                                 &mut condition_status_capture_spans,
                             );
+                            previous_condition = condition;
                         }
 
                         if let Some(else_branch) = &command.else_branch {
-                            let fallback_condition = command
-                                .elif_branches
-                                .last()
-                                .map(|(condition, _)| condition)
-                                .unwrap_or(&command.condition);
                             collect_condition_status_capture_from_body(
-                                fallback_condition,
+                                previous_condition,
                                 else_branch,
                                 self.source,
                                 &mut condition_status_capture_spans,
@@ -8960,6 +8963,14 @@ fn collect_status_parameter_spans_in_command(
                     collect_status_parameter_spans_in_stmt(first_stmt, source, spans);
                 }
             }
+            CompoundCommand::Case(command) => {
+                collect_status_parameter_spans_in_word(&command.word, source, spans);
+                for case in &command.cases {
+                    if let Some(first_stmt) = case.body.first() {
+                        collect_status_parameter_spans_in_stmt(first_stmt, source, spans);
+                    }
+                }
+            }
             CompoundCommand::Subshell(body) | CompoundCommand::BraceGroup(body) => {
                 if let Some(first_stmt) = body.first() {
                     collect_status_parameter_spans_in_stmt(first_stmt, source, spans);
@@ -8989,7 +9000,6 @@ fn collect_status_parameter_spans_in_command(
             | CompoundCommand::Repeat(_)
             | CompoundCommand::Foreach(_)
             | CompoundCommand::ArithmeticFor(_)
-            | CompoundCommand::Case(_)
             | CompoundCommand::Select(_)
             | CompoundCommand::Arithmetic(_) => {}
         },
@@ -16483,10 +16493,17 @@ if [[ -f foo ]]; then
   echo $?
 elif [[ -f bar ]]; then
   echo $?
+elif [ $? -eq 1 ]; then
+  :
 fi
 while test -f baz; do
   echo $?
 done
+if [[ -n $mode ]]; then
+  case $mode in
+    foo) tend $? ;;
+  esac
+fi
 if $(printf one); then
   :
 fi
@@ -16510,7 +16527,7 @@ done
                     .iter()
                     .map(|span| span.slice(source))
                     .collect::<Vec<_>>(),
-                vec!["$?", "$?", "$?"]
+                vec!["$?", "$?", "$?", "$?", "$?"]
             );
         });
     }
